713ba65b4d9d239dc352e37ee98f790abe5877d9
varshadhariya <varshadhariya6>
2021-06-07 11:48:14 +0530

    First commited Version

---
+++ src/main/java/Group1/Artifact1/App.java
@@ -0,0 +1,7 @@
+package Group1.Artifact1;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Group1/Artifact1/AppTest.java
@@ -0,0 +1,15 @@
+package Group1.Artifact1;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
